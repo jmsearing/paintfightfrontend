caad525b1c8efb1d92b6496cee3818b81b97564e
jmsearing <jmsearing@gmail.com>
2024-04-21 18:54:52 -0700

    Routes set up

---
+++ src/components/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            Your mom logs in....
+        </div>
+    )
+}
+
+export default Login
---
+++ src/container/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Your mom
+        </div>
+    )
+}
+
+export default Home
